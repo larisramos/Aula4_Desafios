85d1b32258004591d124ed6ef5b0114b43a22de5
Larissa Ramos <68797820+larisramos@users.noreply.github.com>
2022-03-03 15:36:00 +0000

    Finalizado

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-nome = input('Qual o seu nome? \n')
-print('Olá, '+ nome +'! Prazer em te conhecer!')
+#------------------DESAFIO 1--------------------
+'''nome = input('Qual o seu nome? \n')
+print('Olá, '+ nome +'! Prazer em te conhecer!')'''
+#--------OBS:TIRAR ASPAS PARA RODAR-----------------
+#==================DESAFIO 2=======================
+'''dia = input('Qual dia você nasceu? \n')
+mes = input ('Digite o mês do seu nascimento \n')
+ano = input ('Digite o ano do seu nascimento \n')
+print ('Você nasceu no dia ', dia, ' de ', mes, ' de ', ano, '.Correto?')'''
+#===================DESAFIO 03=====================
+a = int(input('Digite o primeiro número: \n'))
+b = int(input ('Digite o segundo número: \n'))
+resultado = a + b
+print ('A soma é: {}'.format(resultado))
